c6317622f101b870759c04e36941d131c13f3d3e
unknown <vorovencirares18@stud.ase.ro>
2020-10-29 23:17:20 +0200

    Tema1

--- main/app.js
+++ main/app.js
@@ -1,5 +1,28 @@
-function removeOrderItem(orderInfo, position){
+function removeOrderItem(orderInfo , position){
+    if(!(orderInfo.items instanceof Array))
+     throw "Items should be an array";
     
+     orderInfo.items.forEach(item => {
+        if (!item.hasOwnProperty('price') || !item.hasOwnProperty('quantity')) {
+            throw 'Malformed item'
+        }
+    })
+    
+    if(orderInfo.items[position]==undefined)
+     throw "Invalid position";
+    
+    let removed = orderInfo.items.splice(position, 1);
+    
+    let suma = 0
+    for(let i =0; i < orderInfo.items.length; i++){
+	if(orderInfo.items[i].price != undefined && orderInfo.items[i].quantity != undefined){
+		suma = suma + orderInfo.items[i].price * orderInfo.items[i].quantity
+	}
+}
+    
+    orderInfo.total=suma;
+
+    return orderInfo;
 }
 
 const app = {
